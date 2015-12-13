aa2db556f5d87f9926288aeab0abaef22a88c784
Matt Ma <bigmabig@gmail.com>
2015-12-12 21:17:27 -0800

    make header dropdown menu on user info

--- app/components/main-navigation/component.js
+++ app/components/main-navigation/component.js
@@ -8,17 +8,27 @@ export default Component.extend({
   tagName: 'nav',
   classNames: ['main-container', 'main-navigation'],
 
+  // show/hide User dropdown menu
+  showUserDropdown: false,
+
   // targetObject: required keyword to send action to parent component
   // start-non-standard
   @alias('parentView') targetObject,
   // end-non-standard
 
   actions: {
+    // Need to remove, here for education purpose
     // parent component will handle "logoutAction"
     logoutAction () {
       // it has to update "action" property to be able to `sendAction`
       this.set('action', 'logoutAction');
       this.sendAction();
+    },
+
+    toggleUserDropdown () {
+      this.get('showUserDropdown') ?
+        this.set('showUserDropdown', false) :
+        this.set('showUserDropdown', true);
     }
   }
 });
